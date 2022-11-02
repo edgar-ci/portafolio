6360e58d2742e719c71b839397d4b587051eb0d6
Edgar AG <edgarlvzci@gmail.com>
2022-11-02 01:58:29 -0600

    favicon

--- src/components/social-links.js
+++ src/components/social-links.js
@@ -15,14 +15,21 @@ const BtnSocial = ({ className, icon, link }) => (
 const SocialLinks = () => {
   return (
     <div className="social-links">
-      <BtnSocial icon="twitter" className="tw" />
-      <BtnSocial icon="facebook" className="fb" />
+      <BtnSocial
+        icon="twitter"
+        className="tw"
+        link="https://twitter.com/EdgarAlbertoA16"
+      />
       <BtnSocial
         icon="linkedin-in"
         className="lk"
         link="https://www.linkedin.com/in/edgar-alberto-alvarez-garcia-10341292"
       />
-      <BtnSocial icon="instagram" className="in" />
+      <BtnSocial
+        icon="instagram"
+        className="in"
+        link="https://www.instagram.com/fs.dgr"
+      />
       <a
         className="btn btn-social envelope"
         href="mailto:edgarlvzci@gmail.com"
